bc31e94d49da583be26ce1276fff26c1313ec82e
venky-123-debug <venky26494@yahoo.com>
2023-02-17 22:10:35 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import DargAndDrop from "./dargAndDrop.svelte"
+
+  let fileBlobUrl = null
+  const handleDrop = (e) => {
+    fileBlobUrl = URL.createObjectURL(e.detail.file)
+  }
+</script>
+
+<DargAndDrop on:onChange={handleDrop} />
+<img src={fileBlobUrl} alt="" />
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
